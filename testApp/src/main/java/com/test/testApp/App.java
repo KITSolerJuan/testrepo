ec0d82d20a5906f066d66780760387060bd0e43e
solerused_j1 <juan.solerused@kingfisher.com>
2016-03-24 11:28:20 +0100

    test

--- testApp/src/main/java/com/test/testApp/App.java
+++ testApp/src/main/java/com/test/testApp/App.java
@@ -4,10 +4,10 @@ package com.test.testApp;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		// test
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
 }
